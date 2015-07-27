cf306682abdf539c9f5391ca9f2d576d55ba5e27
Carolina Borim <cborim@thoughtworks.com>
2015-07-26 23:27:59 -0300

    editing controller to enable entries creation

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,9 @@
 class EntriesController < ApplicationController #EntriesController herda de ApplicationController
   def new
   end
+
+  def create
+    render plain: params[:entry].inspect
+  end
+
 end
